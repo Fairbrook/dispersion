79779feb7748ad3bfe2cd655dc44aa0f472eec3c
ArturoCarrilloSolorzano <arturo.carrillo.solorzano@gmail.com>
2020-11-28 15:17:31 -0600

    menu

--- include/Menu.h
+++ include/Menu.h
@@ -2,12 +2,16 @@
 #define MENU_H
 #include <iostream>
 #include <string>
+#include <array>
 #include "FileManager.hpp"
+#include "Propietario.h"
 #define INPUT_FILE "AUTOS_PRO.txt"
+#define DISPERSION_FILE "DISPERSION_PRO.txt"
 class Menu
 {
 private:
     FileManager fileManager;
+    propietario propietarioObj;
 
 public:
     Menu();
--- src/Menu.cpp
+++ src/Menu.cpp
@@ -5,25 +5,39 @@ using namespace std;
 Menu::Menu() {}
 void Menu::startMenu()
 {
+    fileManager.readFile(INPUT_FILE);
+    std::array<propietario*, 100> resultado;
     int opc = 0;
     while (opc != 4)
     {
-        cout << "1- Ejecutar la función de dispersión" << endl;
+        cout << "1- Ejecutar la función de dispersion" << endl;
         cout << "2- Mostrar los resultados de la función" << endl;
-        cout << "3- Mostrar el número de frecuencias" << endl;
+        cout << "3- Mostrar el numero de frecuencias" << endl;
         cout << "4- Salir" << endl;
         cin >> opc;
         switch (opc)
         {
         case 1:
-            fileManager.readFile(INPUT_FILE);
             fileManager.applyDispersion();
             break;
 
         case 2:
+            resultado = fileManager.getDirections();
+           for (int i = 0; i < resultado.size(); i++)
+           {
+                cout<< i << " " << endl;
+               if(resultado[i] != nullptr){
+                    cout<< resultado[i]->getPlaca() << endl;
+                    cout<< resultado[i]->getNombre() << endl;
+                    cout<< resultado[i]->getDomicilio() << endl;
+                    cout<< resultado[i]->getProvincia() << endl;
+               }
+           }
+           //fileManager.saveFile(DISPERSION_FILE);
             break;
 
         case 3:
+            fileManager.getFrequency();
             break;
 
         default:
